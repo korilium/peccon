db2dcd9408e928f01c8c04af043f5ea67809ce05
korilium <73854608+korilium@users.noreply.github.com>
2023-02-08 23:27:43 +0100

    final adaptation mpt before review

--- coding/julia/portfolio_optimization.jl
+++ coding/julia/portfolio_optimization.jl
@@ -8,61 +8,53 @@ using AlphaVantage,
      Plots
 
 
-client = AlphaVantage.GLOBAL[]
-client.key = "0VS2G38H6PKP03GX"
-AlphaVantage.GLOBAL[]
+ Ticker = ["SPY", "ADAEUR", "VWCE.DE", "EUNH.DE", "GUNR", 
+            #"VNQ"
+            ]
 
-Tickers = ["SPY", "ADAEUR", "VWCE.DE", "EUNH.DE", "GUNR", "VNQ"]
-days = 750
-SP500 = DataFrame(time_series_daily_adjusted("SPY", outputsize= "full"))
-ADA = DataFrame(time_series_daily_adjusted("ADAEUR", outputsize= "full"))
-ALL_WORLD = DataFrame(time_series_daily_adjusted("VWCE.DE", outputsize= "full"))
-GOV_BOND = DataFrame(time_series_daily_adjusted("EUNH.DE", outputsize= "full"))
-RES = DataFrame(time_series_daily_adjusted("GUNR", outputsize= "full"))
-REAL_EST = DataFrame(time_series_daily_adjusted("VNQ", outputsize= "full"))
+function fin_data(Tickers, days= 500, clientKey = "0VS2G38H6PKP03GX" )
 
-
-
-
-
-
-SP500   = SP500[1:500, :] 
-ADA  = ADA[1:500,:]
-ALL_WORLD = ALL_WORLD[1:500,:]
-GOV_BOND =  GOV_BOND[1:500,:]
-RES = RES[1:500,:]
-REAL_EST = REAL_EST[1:500,:] 
-
-portfolio = [ SP500,
-              ADA, 
-                ALL_WORLD,
-                GOV_BOND, 
-                RES,
-                REAL_EST
-                ]
-
-#calculate returns for each stock 
-for x in portfolio 
-    price = x[!,"close"]
-    returns = zeros(0)
-    for i=2:length(price) 
-        r = log(price[i]/price[i-1])
-        append!(returns, r)
+    #extract the data 
+    client = AlphaVantage.GLOBAL[]
+    client.key = clientKey
+    AlphaVantage.GLOBAL[]
+    portfolio = []
+    for i in Tickers 
+        asset = DataFrame(time_series_daily_adjusted( i, outputsize= "full"))
+        asset = asset[1:days,:]
+        push!(portfolio, asset)
     end 
-    prepend!(returns, 0)
-    x[!,"returns"] = returns 
-end
-
-#add all returns into one dataset 
-port_returns = DataFrame()
-for (x,y) in zip(portfolio, Tickers)
-    port_returns[!,y] = x[!,"returns"]
+    return portfolio
 end 
 
-# calculate standard deviation and covariance of the returns 
-std_dev = std.(eachcol(port_returns))
-covar = cov(Matrix(port_returns))
-corr = cor(Matrix(port_returns))
+
+portfolio = fin_data( Ticker)
+
+function calc_returns(portfolio)
+    #calculate returns for each stock 
+    for x in portfolio 
+        price = x[!,"close"]
+        returns = zeros(0)
+
+        for i=2:length(price) 
+            r = log(price[i]/price[i-1])
+            append!(returns, r)
+        end 
+        #add null in the beginning of each column 
+        prepend!(returns, 0) 
+        x[!,"returns"] = returns # add to dataframe
+    end
+
+    #add all returns into one dataset 
+    port_returns = DataFrame()
+    for (x,y) in zip(portfolio, Tickers)
+        port_returns[!,y] = x[!,"returns"]
+    end 
+    return port_returns
+end 
+
+
+port_returns = calc_returns(portfolio)
 
 #= 
 make random allocation on weights to see the trade-off between risk and return 
@@ -70,55 +62,67 @@ Next plot all possible allocation to see total impact
 In the end find optimal risk return combination  
 =# 
 
+function mpt(returns, simulations= 5000 )
 
-port = DataFrame(exp_return = Float64[],
-                    port_var = Float64[], 
-                    w_SPY = Float64[],
-                    w_ADAEUR = Float64[],
-                    w_VWCE = Float64[],
-                    w_EUNH = Float64[],
-                    w_GUNR = Float64[],
-                    w_VNQ = Float64[]
+    names_stock= names(port_returns)
+    port = DataFrame(exp_return = Float64[],
+                    port_var = Float64[]
                     )
+    for i in names_stock
+        port[:,"weight_"*i]= Float64[]
+    end
 
 
-i = 1;
+    i = 1;
 
-while i <= 50000
-    #set weights 
-    weights = rand(length(portfolio))
-    total = sum(weights)
+    while i <= simulations
+        #set weights 
+        weights = rand(size(port_returns)[2])
+        total = sum(weights)
 
-    w = weights/total
+        w = weights/total
+        Σ = cov(Matrix(port_returns))
+        #calculate returns of the portfolio 
+        port_return = Matrix(port_returns)*w
 
-    #calculate returns of the portfolio 
-    port_return = Matrix(port_returns)*w
+        expected_return = mean(port_return)*250
 
-    expected_return = mean(port_return)*250
+        #calculate variance of the profolio 
+        σ²= 0
+        for i in eachindex(w), j in eachindex(w)
+            x = w[i]*w[j]*Σ[i,j]
+            σ² +=x 
+        end 
 
-    #calculate variance of the profolio 
-    σ = 0
-    for i in eachindex(w), j in eachindex(w)
-        x = w[i]*w[j]*covar[i,j]
-        σ+=x 
+        port_var = (σ²*250)
+
+        list = [expected_return, port_var, w]
+        #decompose 
+        results = collect(Iterators.flatten(list))
+
+        push!(port, results )
+        i += 1
     end 
 
-    port_var = √(σ*250)
-
-    list = [expected_return, port_var, w]
-    #decompose 
-    results = collect(Iterators.flatten(list))
-
-    push!(port, results )
-    i += 1
+    port[:,:port_std] = .√port[:,:port_var]
+    return port
 end 
 
+port_sim = mpt(port_returns)
+@df port_sim scatter(:port_std, :exp_return)
+#calculate the sharp ratio 
+function sharp_ration(port_sim, rf = 0.02)
+    port_sim[:, :sharp_ratio] = (port_sim[:,:exp_return] .- rf )./port_sim[: , :port_std]
+    return sort!(port_sim, :sharp_ratio)
+end 
 
-@df port scatter(:port_var, :exp_return)
+sharp_ration(port_sim)
 
+#utility function σ² - qE(Rₚ)
+function utility_mpt(port_sim, q = 0 )
 
-rf = 0.0461
+    port_sim[:,:utility] = abs.(port_sim[:,:port_var] - q*port_sim[:,:exp_return])
+    return sort!(port_sim,:utility)
+end 
 
-port[:, :sharp_ratio] = (port[:,:exp_return] .- rf )./port[: , :port_var]
-
-sort!(port, :sharp_ratio)
+utility_mpt(port_sim)
